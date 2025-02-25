72a7007c5119480dd1caf5cdcce811394bcfd7df
pienik <141414327+pienik@users.noreply.github.com>
2025-02-25 13:22:36 +0100

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './loop-learning-game.jsx',
+  output: {
+    filename: 'loop-learning-game.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
