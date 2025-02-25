619e00b087a16130f3753ef8fd0f482a8e2d9a91
pienik <141414327+pienik@users.noreply.github.com>
2025-02-25 16:52:29 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LoopMasterGame from './loop-learning-game';
+
+ReactDOM.render(<LoopMasterGame />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',  // Zmień ścieżkę wejściową
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
